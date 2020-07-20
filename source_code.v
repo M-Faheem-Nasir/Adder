a40c78f4827a4d08c3e10dcd218a1e76d6f29347
M-Faheem-Nasir <faheem.mfn@gmail.com>
2020-07-20 15:36:10 +0500

    formated source code module

--- source_code.v
+++ source_code.v
@@ -13,15 +13,12 @@ module rca_pipe_2bit(a,b,cin,clk,sum,cout);
   reg [10:0] pr6;		//pipeline reg 6 for stage 6
   reg [9:0]  pr7;		//pipeline reg 7 for stage 7
   reg [8:0]  OR;		//output reg to store output of last stage
-
   assign {cout,sum} = OR;	//assigning content of output reg to our outputs
-
   always @(posedge clk) begin
   	//mapping our inputs to bits of input registers so that we can easily use them i.e {a,b,c} -> input reg at a posedge of clock
     ir[0] <= cin;
     ir[1] <= a[0];ir[3] <= a[1];ir[5] <= a[2];ir[7] <= a[3];ir[9] <= a[4];ir[11] <= a[5];ir[13] <= a[6];ir[15] <= a[7];
     ir[2] <= b[0];ir[4] <= b[1];ir[6] <= b[2];ir[8] <= b[3];ir[10] <=b[4];ir[12] <= b[5];ir[14] <= b[6];ir[16] <= b[7];
-
     //forwarding the pipeline stage data to next stage at a posedge of clock
     pr1[15:2]<= ir[16:3];
     pr2[14:3]<= pr1[15:4];	pr2[0]		<= pr1[0];
@@ -30,7 +27,6 @@ module rca_pipe_2bit(a,b,cin,clk,sum,cout);
     pr5[11:6]<= pr4[12:7];	pr5[3:0]	<= pr4[3:0];
     pr6[10:7]<= pr5[11:8];	pr6[4:0]	<= pr5[4:0];
     pr7[9:8] <= pr6[10:9];	pr7[5:0]	<= pr6[5:0];
-
     //Performing addition on each stage at a posedge of clock
     pr1[1:0] <= ir[1]  +ir[2]  + ir[0];
     pr2[2:1] <= pr1[3] +pr1[2] + pr1[1];
@@ -39,11 +35,7 @@ module rca_pipe_2bit(a,b,cin,clk,sum,cout);
     pr5[5:4] <= pr4[6] +pr4[5] + pr4[4];
     pr6[6:5] <= pr5[7] +pr5[6] + pr5[5];
     pr7[7:6] <= pr6[9] +pr6[8] + pr6[6];
-    
     //Performing addition at last stage and putting our output to our output register i.e OR
     OR		 <= { pr7[9]+pr7[8]+pr7[7],pr7[6:0]};
-
   end
-
-
  endmodule
